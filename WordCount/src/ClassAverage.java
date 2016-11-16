0c7c52f2d8575a5f2a97a6e5b10a0381473d7fda
Ruben <santipab.stephane@gmail.com>
2016-11-16 12:44:44 -0600

    Doodling in class

--- WordCount/src/ClassAverage.java
+++ WordCount/src/ClassAverage.java
@@ -1,7 +1,10 @@
 
+import java.io.IOException;
+import java.util.StringTokenizer;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,9 +18,42 @@ import org.apache.hadoop.mapreduce.Mapper;
  */
 public class ClassAverage {
     
-    private static class TokenizerMapper extends Mapper<Object, Long, Long, IntWritable>
+    private static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>
     {
+        private final static IntWritable one = new IntWritable(1);
         
+        private Double val = 0.0;
+        
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                //word.set(itr.nextToken());
+                //context.write(word, one);
+            }
+        }    
     }
     
+    public static class DoubleCalcReducer
+            extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                Reducer.Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+    
+    }
 }
+
+
